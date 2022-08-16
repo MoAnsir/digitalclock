ae382ecda1b0e7004ed6852e07efc1e368e0ca82
Mohammed Ansir <1854992+MoAnsir@users.noreply.github.com>
2022-08-16 21:31:17 +0100

    cypress tests

--- cypress/e2e/1-digital-clock/digitalClock.cy.js
+++ cypress/e2e/1-digital-clock/digitalClock.cy.js
@@ -1,7 +1,40 @@
 /// <reference types="cypress" />
 
 describe("digital clock", () => {
-  it("test 1 ", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000/");
+  });
+
+  it("displays the default view - day, time, switch", () => {
+    cy.get(".clock__day").should("exist");
+    cy.get(".clock__timer").should("exist");
+    cy.get(".clock__24h-switch").should("exist");
+  });
+
+  it("displays the correct day", () => {
+    const d = new Date();
+    const day = d.getDay();
+    const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+    cy.get(".clock__day").should("have.text", days[day]);
+  });
+
+  it("displays the correct time", () => {
+    const d = new Date();
+    const hour = d.getHours();
+    const minute = d.getMinutes();
+    const sec = d.getSeconds();
+
+    const minuteFormat = minute < 10 ? "0" + minute : minute;
+    const secondFormat = sec < 10 ? "0" + sec : sec;
+    const timeFormat = `${hour}:${minuteFormat}:${secondFormat}`;
+
+    cy.get(".clock__timer").should("have.text", timeFormat);
+  });
+
+  it("change from 24h clock to 12h clock", () => {
+    cy.get(".clock__24h-switch input").should("exist");
+    cy.get(".clock__24h-switch input").click({ force: true });
+    cy.get(".clock__timer").should("include.text", "PM");
   });
 });
